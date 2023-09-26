3b0936efeed8ad5a786f5240ae998fa190bdf9e8
Mikus Kukainis <mikuskukainis@gmail.com>
2023-09-26 19:15:26 +0300

    Added design to login and register components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './style.scss'
+import Register from './components/Register';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Register />
+  );
+}
+
+export default App;
